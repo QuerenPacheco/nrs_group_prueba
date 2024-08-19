f1422129ee4b7d21e889f3c94766373f1b8f8395
Queren <querenpachecocastro@gmail.com>
2024-08-19 19:49:43 +0200

    modify layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,49 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     @vite('resources/js/app.js')</head>
 <body>
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+        .navbar {
+            background-color: #fff;
+            color: #0094de;
+            padding: 10px 20px;
+            position: fixed;
+            width: 100%;
+            top: 0;
+            left: 0;
+            z-index: 1000;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+        }
+        
+        
+        .navbar a {
+            color: #0094de;
+            text-decoration: none;
+            padding: 14px 20px;
+            display: inline-block;
+            font-size: 16px;
+        }
+        .navbar a:hover {
+            background-color: #0094de;
+            color:white;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+
+        }
+        .container {
+            margin-top: 100px;
+        }
+    </style>
+    <div class="navbar">
+        <a href="{{ route('inicio') }}">Inicio</a>
+        <a href="{{ route('clientes.index') }}">Clientes</a>
+        <a href="{{ route('proveedores.index') }}">Proveedores</a>
+    </div>
     <div class="container">
         @yield('content')
     </div>
